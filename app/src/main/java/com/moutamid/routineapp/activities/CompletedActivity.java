fa5dcfd5863e8350115775176bcde901522ff49e
suleman81 <sunnybajwa904@gmail.com>
2023-10-23 18:35:34 +0500

    commit

--- app/src/main/java/com/moutamid/routineapp/activities/CompletedActivity.java
+++ app/src/main/java/com/moutamid/routineapp/activities/CompletedActivity.java
@@ -49,7 +49,7 @@ public class CompletedActivity extends AppCompatActivity {
             Stash.put(Constants.IS_VIP, false);
             Ads.init(this);
             Ads.showBanner(binding.adView);
-            Ads.loadIntersAD(this);
+//            Ads.loadIntersAD(this);
         } else {
             binding.adView.setVisibility(View.GONE);
         }
